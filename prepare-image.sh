0f5bf54661d7b0359590f3b0823e18a858c05e2c
Vũ Huy Phát <phatvhup@gmail.com>
2022-09-14 09:52:48 +0700

    master-master mysql

--- prepare-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mysql_dirs="/var/lib/mysql /var/lib/mysql-files /var/lib/mysql-keyring /var/run/mysqld"
-
-for dir in $mysql_dirs; do
-    mkdir -p $dir
-    chmod g+rwx $dir
-    chgrp -R 0 $dir
-done
